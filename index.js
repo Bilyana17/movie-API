a5e7a48d19667e1e06b53e955c1fab2af93317c6
Bilyana17 <biljana.bicka@gmail.com>
2025-01-29 20:26:42 -0600

    config vars

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 const Movies = Models.Movie;
-const Users = Models.Users;
+const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/test', {
+/* mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});*/
+
+mongoose.connect('process.env.CONNECTION_URI', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
