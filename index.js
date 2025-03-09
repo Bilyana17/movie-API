14d33571b24cafecfe6a49d4dce4e381ca35f959
Bilyana17 <biljana.bicka@gmail.com>
2025-03-09 18:30:58 -0500

    connection set

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ const Users = Models.User;
 
 //Connect to MongoD
 
-mongoose.connect('mongodb://localhost:27017/test', {
+mongoose.connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+/*mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});*/
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
